94e6659d7f49be6654045aae145364c58d9bdd07
Rodrigo Waters <rdrgwtrs@gmail.com>
2020-01-16 22:51:42 -0300

    Arquivos iniciais

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Header/>
+    <main class="container-fluid">
+      <aside>
+        <ul>
+          <li><a href="#page-1">Page 1</a></li>
+          <li><a href="#page-2">Page 2</a></li>
+          <li><a href="#page-3">Page 3</a></li>
+          <li><a href="#page-4">Page 4</a></li>
+        </ul>
+      </aside>
+      <section>
+        <router-view/>
+      </section>
+    </main>
+    <Footer/>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+    <!--
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    -->
+  </div>
+</template>
+
+<script>
+
+  import Header from '@/components/Header.vue'
+  import Footer from '@/components/Footer.vue'
+
+  export default {
+    name: 'home',
+    assetsDir: 'assets/',
+    components: {
+      Header, Footer
+    }
+  }
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <header id="header" class="container-fluid" role="banner">
+    <div class="row">
+      <div class="col">
+        <ul role="menubar">
+          <li role="menuitem"><a href="#">Ir para o conteúdo</a></li>
+          <li role="menuitem"><a href="#">Ir para o menu</a></li>
+          <li role="menuitem"><a href="#">Ir para a busca</a></li>
+          <li role="menuitem"><a href="#">Ir para o rodapé</a></li>
+        </ul>
+      </div>
+      <div class="col">
+        <ul class="menu-borda" role="menubar">
+          <li role="menuitem"><a href="#">A+</a></li>
+          <li role="menuitem"><a href="#">A-</a></li>
+          <li role="menuitem"><a href="#">ACESSIBILIDADE</a></li>
+          <li role="menuitem"><a href="#">ALTO CONSTRATE</a></li>
+          <li role="menuitem"><a href="#">MAPA DO SITE</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col logo">
+        <h1><a href="#">Portal da Transpar<strong>ê</strong>ncia</a></h1><span>Prefeitura Municipal de São Leopoldo</span></div>
+    </div>
+    <div class="row" id="busca-rapida">
+      <div class="col"></div>
+      <div class="col">
+        <form>
+          <div class="input-group">
+            <div class="input-group-prepend"><span class="input-group-text">Procurar</span></div><input class="form-control" type="text">
+            <div class="input-group-append"><button class="btn btn-primary" type="button"><i class="fa fa-search" style="font-size: 14px;"></i></button></div>
+          </div>
+        </form>
+      </div>
+      <div class="col"></div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
