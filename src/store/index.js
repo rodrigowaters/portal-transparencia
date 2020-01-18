bded75bc8617bca99e77fcc127c361e89434d0e7
Rodrigo Waters <rdrgwtrs@gmail.com>
2020-01-18 13:00:38 -0300

    Suporte a Mapa do Site

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,19 @@
-import Vue  from 'vue'
-import Vuex from 'vuex'
+import Vue      from 'vue'
+import Vuex     from 'vuex'
+import menuJson from '../menu.json'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state    : {},
+    state    : {
+        menus: menuJson
+    },
+    getters  : {
+        menus: state =>
+        {
+            return state.menus;
+        }
+    },
     mutations: {},
     actions  : {},
     modules  : {}
--- src/views/Mapa-Do-Site.vue
+++ src/views/Mapa-Do-Site.vue
@@ -1,11 +1,18 @@
 <template>
     <div>
         Mapa do Site
+        {{ menus }}
     </div>
 </template>
 
 <script>
     export default {
         name: 'mapa-do-site',
+        data()
+        {
+            return {
+                menus: this.$store.getters.menus
+            }
+        }
     }
 </script>
