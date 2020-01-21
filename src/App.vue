23086e12d7c3231e8aa2a5780467c3dce3aeca7d
Rodrigo Silva <rodrigo.silva@multi24h.com.br>
2020-01-21 15:01:29 -0300

    fix - build

--- src/App.vue
+++ src/App.vue
@@ -41,8 +41,10 @@
       this.$Progress.start();
 
       //
-      this.$router.beforeEach((to, from, next) => {
-        if (to.meta.progress !== undefined) {
+      this.$router.beforeEach((to, from, next) =>
+      {
+        if (to.meta.progress !== undefined)
+        {
           let meta = to.meta.progress;
           this.$Progress.parseMeta(meta)
         }
@@ -51,8 +53,10 @@
       });
 
       //
-      this.$router.afterEach((to, from) => {
-        console.log(from.path, ' -> ', to.path);
+      this.$router.afterEach(() =>
+      {
+        // to, from
+        // console.log(from.path, ' -> ', to.path);
         this.$Progress.finish();
       });
 
